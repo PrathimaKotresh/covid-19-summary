15b97ea31d0f38c190ea5bc64b2f8ca41259cceb
Prathima Kotresh <prathima.kotresh@gmail.com>
2020-05-28 17:57:08 +0200

    added error message and code cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,9 @@ var covid19Repository = (function() {
     return covid19CountriesList;
   }
 
-  function addGlobalData() {
+  function addGlobalDataTableStructure() {
     // global details division
     //create a table to display global details
-    var virusImage = $('<img src="./img/virus.png" class="virusImage" alt="image of corona virus">');
     var $table = $('<table>');
     $table.append('<caption></caption>')
       // thead
@@ -35,7 +34,6 @@ var covid19Repository = (function() {
 
     // add table to dom
     $('.work1').append($table);
-    $('.work1').append(virusImage);
   }
 
   function createCountryTableStructure() {
@@ -57,14 +55,14 @@ var covid19Repository = (function() {
 
   // funtion to create list of button to each country in frontend
   function addListItem(index, country) {
-    var markup = "<tr><td>" + country.Country + "</td><td>" + country.TotalConfirmed + "</td><td><button class=\"moreDetailsButton" + index +"\">Click</button></td></tr>";
+    var markup = "<tr><td>" + country.Country + "</td><td>" + country.TotalConfirmed + "</td><td><button class=\"moreDetailsButton" + index + "\">Click</button></td></tr>";
     $(".countryTable tbody").append(markup);
     clickShowDetailsButton(index, country);
   }
 
   // function to handle country button click to show country details
   function clickShowDetailsButton(index, country) {
-    $('.moreDetailsButton'+index).on('click', function(event) {
+    $('.moreDetailsButton' + index).on('click', function(event) {
       showDetails(country);
     });
   }
@@ -93,34 +91,34 @@ var covid19Repository = (function() {
     closeButtonElement.on('click', hideModal);
 
     // add title element
-    var titleElement = $('<h1>'+ title+ '</h1>');
+    var titleElement = $('<h1>' + title + '</h1>');
 
     // add country name element
-    var countryElement = $('<p>Country: ' + country.Country+ '</p>');
+    var countryElement = $('<p>Country: ' + country.Country + '</p>');
 
     // add total confirmed cases element
-    var totalConfirmedElement = $('<p>Total confirmed: ' + country.TotalConfirmed+ '</p>');
+    var totalConfirmedElement = $('<p>Total confirmed: ' + country.TotalConfirmed + '</p>');
 
     // add new confirmed cases element
-    var newConfirmedElement = $('<p>New confirmed: ' + country.NewConfirmed+ '</p>');
+    var newConfirmedElement = $('<p>New confirmed: ' + country.NewConfirmed + '</p>');
 
     // add total deaths element
-    var totalDeathsElement = $('<p>Total Deaths: ' + country.TotalDeaths+ '</p>');
+    var totalDeathsElement = $('<p>Total Deaths: ' + country.TotalDeaths + '</p>');
 
     // add new deaths element
-    var newDeathsElement = $('<p>New Deaths: ' + country.NewDeaths+ '</p>');
+    var newDeathsElement = $('<p>New Deaths: ' + country.NewDeaths + '</p>');
 
     // add total recovered element
-    var totalRecoveredElement = $('<p>Total Recovered: ' + country.TotalRecovered+ '</p>');
+    var totalRecoveredElement = $('<p>Total Recovered: ' + country.TotalRecovered + '</p>');
 
     // add new recovered element
-    var newRecoveredElement = $('<p>New Recovered: ' + country.NewRecovered+ '</p>');
+    var newRecoveredElement = $('<p>New Recovered: ' + country.NewRecovered + '</p>');
 
     // add country image element
-    var imageElement = $('<img src=\"https://www.countryflags.io/'+ country.CountryCode + '/flat/64.png\"' +'width=\"200\" height=\"200\" alt=' +country.Country + '>');
+    var imageElement = $('<img src=\"https://www.countryflags.io/' + country.CountryCode + '/flat/64.png\"' + 'width=\"200\" height=\"200\" alt=' + country.Country + '>');
 
     // add last updated element
-    var dateElement = $('<p>Last updated:'  + country.Date + '</p>');
+    var dateElement = $('<p>Last updated:' + country.Date + '</p>');
     $modal.append(closeButtonElement);
     $modal.append(titleElement);
     $modal.append(countryElement);
@@ -171,12 +169,20 @@ var covid19Repository = (function() {
     showModal('COVID-19', item);
   }
 
+  function addErrorMessage() {
+    var error = $('.error');
+    var para = $('<p>Data not available at the moment! Please try again later...</p>');
+    error.append(para);
+  }
+
   // function to fetch country list from backend api and add to country list
   function loadList() {
     showLoadingMessage();
-    return $.ajax(apiUrl, { dataType: 'json' }).then(function(response) {
+    return $.ajax(apiUrl, {
+      dataType: 'json'
+    }).then(function(response) {
       hideLoadingMessage();
-      globalData = response.Global
+      globalData = response.Global;
       response.Countries.forEach(function(item) {
         var country = {
           Country: item.Country,
@@ -191,8 +197,11 @@ var covid19Repository = (function() {
         };
         add(country);
       });
+      addGlobalDataTableStructure();
+      createCountryTableStructure();
     }).catch(function(e) {
       hideLoadingMessage();
+      addErrorMessage();
       console.error(e);
     })
   }
@@ -203,11 +212,6 @@ var covid19Repository = (function() {
     return fetch(url).then(function(response) {
       hideLoadingMessage();
       return response.json();
-    }).then(function(details) {
-      // Now we add the details to the item
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.types = details.types;
     }).catch(function(e) {
       hideLoadingMessage();
       console.error(e);
@@ -220,15 +224,11 @@ var covid19Repository = (function() {
     addListItem: addListItem,
     loadList: loadList,
     loadDetails: loadDetails,
-    addGlobalData: addGlobalData,
-    createCountryTableStructure: createCountryTableStructure
   };
 })();
 
 // to add country to buttons
 covid19Repository.loadList().then(function() {
-  covid19Repository.addGlobalData()
-  covid19Repository.createCountryTableStructure()
   // Now the data is loaded!
   covid19Repository.getAll().forEach(function(country, index) {
     covid19Repository.addListItem(index, country);
@@ -238,3 +238,6 @@ covid19Repository.loadList().then(function() {
 // Header part
 var headerTitle = $('<h1 class="headerTitle">COVID-19 Tracker</h1>');
 $('.header').append(headerTitle);
+
+var virusImage = $('<img src="./img/virus.png" class="virusImage" alt="image of corona virus">');
+$('.work1').append(virusImage);
